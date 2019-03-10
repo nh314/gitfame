8190bab5e655f426585338c6846b48216ca86f35
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-10 17:31:56 +0000

    Deploy nh314/gitfame to github.com/nh314/gitfame.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e89078d9b273222c8a3cc9d179c7b3a7.js"
+  "/gitfame/precache-manifest.81187b5b077acef62d040b6929505c53.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gitfame/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
